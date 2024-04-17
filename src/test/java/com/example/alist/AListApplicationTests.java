3cdf933b4f9c39b06fffe804fbe3b4bf2a510cea
HanTx <247777055@qq.com>
2024-04-17 12:09:04 +0800

    initial

---
+++ src/test/java/com/example/alist/AListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
